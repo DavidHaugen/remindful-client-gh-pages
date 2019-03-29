b612303d54b2794e6d1f8c262d0b811f8e9f1788
David Haugen <haugen.dj@gmail.com>
2019-03-29 10:51:16 -0500

    Updates

---
+++ precache-manifest.bd15b8a74009466bc61c76b7f5f47b66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5651f0ef3973fc9e27f",
+    "url": "/DavidHaugen/remindful-client/static/css/main.7647d47a.chunk.css"
+  },
+  {
+    "revision": "c5651f0ef3973fc9e27f",
+    "url": "/DavidHaugen/remindful-client/static/js/main.a85105d1.chunk.js"
+  },
+  {
+    "revision": "75f95a96b5c412f8d6e4",
+    "url": "/DavidHaugen/remindful-client/static/js/runtime~main.ec391158.js"
+  },
+  {
+    "revision": "4089cd73687c3198ee76",
+    "url": "/DavidHaugen/remindful-client/static/js/2.485d0710.chunk.js"
+  },
+  {
+    "revision": "36f49cc5d8fbe496bd6a99e765de89f1",
+    "url": "/DavidHaugen/remindful-client/index.html"
+  }
+];
--- src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  API_ENDPOINT: 'https://quiet-mountain-96034.herokuapp.com',
-  TOKEN_KEY: 'TOKEN_KEY'
-}
